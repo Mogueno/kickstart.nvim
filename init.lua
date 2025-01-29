20df8db5e9e3201e2cece6c1966df90a0003bb65
Mogueno <buguinha29@gmail.com>
2025-01-29 13:58:46 +0100

    [] added lazygit config

--- init.lua
+++ init.lua
@@ -908,6 +908,9 @@ require('lazy').setup({
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
+    config = function()
+      vim.g.lazygit_floating_window_scaling_factor = 1
+    end,
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
